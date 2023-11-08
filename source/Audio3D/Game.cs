ff86b6420929a53fe25ea9f87ea63759922c2df7
Christopher Whitley <aristurtledev@gmail.com>
2023-11-08 00:36:51 -0500

    Update coding style to common c# conventions

--- source/Audio3D/Game.cs
+++ source/Audio3D/Game.cs
@@ -23,24 +23,24 @@ namespace Audio3D
     {
         #region Fields
 
-        private readonly GraphicsDeviceManager graphics;
+        private readonly GraphicsDeviceManager _graphics;
 
-        private readonly AudioManager audioManager;
+        private readonly AudioManager _audioManager;
 
-        private readonly SpriteEntity cat;
-        private readonly SpriteEntity dog;
+        private readonly SpriteEntity _cat;
+        private readonly SpriteEntity _dog;
 
-        Texture2D checkerTexture;
+        private Texture2D _checkerTexture;
 
-        QuadDrawer quadDrawer;
+        private QuadDrawer _quadDrawer;
 
-        Vector3 cameraPosition = new Vector3(0, 512, 0);
-        Vector3 cameraForward = Vector3.Forward;
-        Vector3 cameraUp = Vector3.Up;
-        Vector3 cameraVelocity = Vector3.Zero;
+        private Vector3 _cameraPosition = new Vector3(0, 512, 0);
+        private Vector3 _cameraForward = Vector3.Forward;
+        private Vector3 _cameraUp = Vector3.Up;
+        private Vector3 _cameraVelocity = Vector3.Zero;
 
-        KeyboardState currentKeyboardState;
-        GamePadState currentGamePadState;
+        private KeyboardState _currentKeyboardState;
+        private GamePadState _currentGamePadState;
 
         #endregion
 
@@ -51,14 +51,14 @@ namespace Audio3D
         {
             Content.RootDirectory = "Content";
 
-            graphics = new GraphicsDeviceManager(this);
+            _graphics = new GraphicsDeviceManager(this);
 
-            audioManager = new AudioManager(this);
+            _audioManager = new AudioManager(this);
 
-            Components.Add(audioManager);
+            Components.Add(_audioManager);
 
-            cat = new Cat();
-            dog = new Dog();
+            _cat = new Cat();
+            _dog = new Dog();
         }
 
 
@@ -67,12 +67,12 @@ namespace Audio3D
         /// </summary>
         protected override void LoadContent()
         {
-            cat.Texture = Content.Load<Texture2D>("CatTexture");
-            dog.Texture = Content.Load<Texture2D>("DogTexture");
+            _cat.Texture = Content.Load<Texture2D>("CatTexture");
+            _dog.Texture = Content.Load<Texture2D>("DogTexture");
 
-            checkerTexture = Content.Load<Texture2D>("checker");
+            _checkerTexture = Content.Load<Texture2D>("checker");
 
-            quadDrawer = new QuadDrawer(graphics.GraphicsDevice);
+            _quadDrawer = new QuadDrawer(_graphics.GraphicsDevice);
         }
 
 
@@ -91,14 +91,14 @@ namespace Audio3D
             UpdateCamera();
 
             // Tell the AudioManager about the new camera position.
-            audioManager.Listener.Position = cameraPosition;
-            audioManager.Listener.Forward = cameraForward;
-            audioManager.Listener.Up = cameraUp;
-            audioManager.Listener.Velocity = cameraVelocity;
+            _audioManager.Listener.Position = _cameraPosition;
+            _audioManager.Listener.Forward = _cameraForward;
+            _audioManager.Listener.Up = _cameraUp;
+            _audioManager.Listener.Velocity = _cameraVelocity;
 
             // Tell our game entities to move around and play sounds.
-            cat.Update(gameTime, audioManager);
-            dog.Update(gameTime, audioManager);
+            _cat.Update(gameTime, _audioManager);
+            _dog.Update(gameTime, _audioManager);
 
             base.Update(gameTime);
         }
@@ -109,16 +109,16 @@ namespace Audio3D
         /// </summary>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice device = graphics.GraphicsDevice;
+            GraphicsDevice device = _graphics.GraphicsDevice;
 
             device.Clear(Color.CornflowerBlue);
 
             device.BlendState = BlendState.AlphaBlend;
 
             // Compute camera matrices.
-            Matrix view = Matrix.CreateLookAt(cameraPosition,
-                                              cameraPosition + cameraForward,
-                                              cameraUp);
+            Matrix view = Matrix.CreateLookAt(_cameraPosition,
+                                              _cameraPosition + _cameraForward,
+                                              _cameraUp);
 
             Matrix projection = Matrix.CreatePerspectiveFieldOfView(1, device.Viewport.AspectRatio,
                                                                     1, 100000);
@@ -127,11 +127,11 @@ namespace Audio3D
             Matrix groundTransform = Matrix.CreateScale(20000) *
                                      Matrix.CreateRotationX(MathHelper.PiOver2);
 
-            quadDrawer.DrawQuad(checkerTexture, 32, groundTransform, view, projection);
+            _quadDrawer.DrawQuad(_checkerTexture, 32, groundTransform, view, projection);
 
             // Draw the game entities.
-            cat.Draw(quadDrawer, cameraPosition, view, projection);
-            dog.Draw(quadDrawer, cameraPosition, view, projection);
+            _cat.Draw(_quadDrawer, _cameraPosition, view, projection);
+            _dog.Draw(_quadDrawer, _cameraPosition, view, projection);
 
             base.Draw(gameTime);
         }
@@ -147,12 +147,12 @@ namespace Audio3D
         /// </summary>
         void HandleInput()
         {
-            currentKeyboardState = Keyboard.GetState();
-            currentGamePadState = GamePad.GetState(PlayerIndex.One);
+            _currentKeyboardState = Keyboard.GetState();
+            _currentGamePadState = GamePad.GetState(PlayerIndex.One);
 
             // Check for exit.
-            if (currentKeyboardState.IsKeyDown(Keys.Escape) ||
-                currentGamePadState.Buttons.Back == ButtonState.Pressed)
+            if (_currentKeyboardState.IsKeyDown(Keys.Escape) ||
+                _currentGamePadState.Buttons.Back == ButtonState.Pressed)
             {
                 Exit();
             }
@@ -169,33 +169,33 @@ namespace Audio3D
             const float frictionAmount = 0.98f;
 
             // Turn left or right.
-            float turn = -currentGamePadState.ThumbSticks.Left.X * turnSpeed;
+            float turn = -_currentGamePadState.ThumbSticks.Left.X * turnSpeed;
 
-            if (currentKeyboardState.IsKeyDown(Keys.Left))
+            if (_currentKeyboardState.IsKeyDown(Keys.Left))
                 turn += turnSpeed;
 
-            if (currentKeyboardState.IsKeyDown(Keys.Right))
+            if (_currentKeyboardState.IsKeyDown(Keys.Right))
                 turn -= turnSpeed;
 
-            cameraForward = Vector3.TransformNormal(cameraForward,
+            _cameraForward = Vector3.TransformNormal(_cameraForward,
                                                     Matrix.CreateRotationY(turn));
 
             // Accelerate forward or backward.
-            float accel = currentGamePadState.ThumbSticks.Left.Y * accelerationSpeed;
+            float accel = _currentGamePadState.ThumbSticks.Left.Y * accelerationSpeed;
 
-            if (currentKeyboardState.IsKeyDown(Keys.Up))
+            if (_currentKeyboardState.IsKeyDown(Keys.Up))
                 accel += accelerationSpeed;
 
-            if (currentKeyboardState.IsKeyDown(Keys.Down))
+            if (_currentKeyboardState.IsKeyDown(Keys.Down))
                 accel -= accelerationSpeed;
 
-            cameraVelocity += cameraForward * accel;
+            _cameraVelocity += _cameraForward * accel;
 
             // Add velocity to the current position.
-            cameraPosition += cameraVelocity;
+            _cameraPosition += _cameraVelocity;
 
             // Apply the friction force.
-            cameraVelocity *= frictionAmount;
+            _cameraVelocity *= frictionAmount;
         }
 
 
